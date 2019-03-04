1cbf890b8cb1b337a297eb7cdb7db2028a6a9aa7
aleks-nek <aleks.nekrashevitch@gmail.com>
2019-03-04 19:23:37 +0300

    user

---
+++ src/main/java/com/epolsoft/practice/user/UserController.java
@@ -0,0 +1,27 @@
+package com.epolsoft.practice.user;
+
+
+import com.epolsoft.practice.payload.UserSummary;
+import com.epolsoft.practice.security.CurrentUser;
+import com.epolsoft.practice.security.UserPrincipal;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @GetMapping("/me")
+//    @PreAuthorize("hasRole('ROLE_USER')")
+    public UserSummary getCurrentUser (@CurrentUser UserPrincipal currentUser){
+        return new UserSummary(
+                currentUser.getId(),
+                currentUser.getUsername(),
+                currentUser.getFirstName(),
+                currentUser.getLastName(),
+                currentUser.getEmail()
+        );
+    }
+}
